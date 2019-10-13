04a35a2e3d1a33bfd82156b5d5d2781e8632622d
Brian <unasbrian@gmail.com>
2019-10-13 00:35:38 -0500

    x

---
+++ src/brianpando/plantumlgen/PlantServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Brianpando\Plantumlgen;
+
+use Illuminate\Support\ServiceProvider;
+
+class PlantServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+        include __DIR__.'/routes/web.php';
+        $this->app->make('Brianpando\Plantumlgen\PlantController');
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot(){
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                Brianpando\Plantumlgen\Commands\PlantMigrations::class,
+                Brianpando\Plantumlgen\Commands\PlantModels::class,
+            ]);
+        }
+    }
+    // public function boot(\Illuminate\Routing\Router $router) {
+    //     $this->commands([
+    //         \Vendor\plantumlgen\Commands\PlantMigrations::class,
+    //         \Vendor\plantumlgen\Commands\PlantModels::class,
+    //     ]);
+    // }
+}
